f5f4cb90d1aac12268617fd56dc1f66c134678d4
RaphTal <raphael.de.talhouet@gmail.com>
2013-08-24 21:56:40 +0200

    Kata Four - First part

--- Kata Four/kataFour.js
+++ Kata Four/kataFour.js
@@ -1,14 +1,32 @@
+/*
+* Kata Four
+* See http://codekata.pragprog.com/2007/01/kata_four_data_.html
+*/
+
 var fs = require('fs')
   , util = require('util')
+  , _ = require('underscore')
+  , assert = require('chai').assert
   ;
 
-function dataToTable() {
+function getDayWithLowestSpread() {
   var data = fs.readFileSync('weather.dat', {encoding: 'utf-8'} )
-    , tableData = data.split('\n');
+    , tableData = data.split('\n').slice(8,38)
+    , pattern = /\d+/g // search for numbers
     ;
-  
-  console.log(util.inspect(tableData));
 
+  return _.chain(tableData)
+    .reduce(function(memo, line){
+        var infos = line.match(pattern);
+        memo.push({number: infos[0], spread: infos[1] - infos[2]});
+        return memo;
+      }, [])
+    .sortBy(function(day){ return day.spread; })
+    .map(function(day){ return day.number; })
+    .first()
+    .value();
 }
 
-dataToTable();
+// test
+assert.equal(14, getDayWithLowestSpread());
+
